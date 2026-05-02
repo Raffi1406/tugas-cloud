f4cca63c156f92574a2a0531c30d850a2f1f842e
Raffi1406 <bukanraffi14@gmail.com>
2026-05-02 14:54:19 +0700

    Fix port for Railway deployment

--- backend/app.py
+++ backend/app.py
@@ -85,4 +85,4 @@ def handle_todos():
 if __name__ == '__main__':
     time.sleep(5)
     init_db()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
